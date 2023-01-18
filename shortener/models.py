3806ebce2b042507d3116dbd5096917e259860f8
Diego Herrera <jdht1992@gmail.com>
2023-01-18 13:12:37 -0600

    added Dockerfile and docker-compose

--- shortener/models.py
+++ shortener/models.py
@@ -1,11 +1,6 @@
-import random
-import string
-
 from django.db import models
 
-
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+from shortener.utils import code_generator
 
 
 class ShortenerURL(models.Model):
